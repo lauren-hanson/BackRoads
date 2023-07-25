21d18a58288ed8d819fa946c1178da0840112932
Lauren Hanson <lohanson33@gmail.com>
2023-07-24 18:37:30 -0600

    tour information dynamically added

--- src/components/Tours.js
+++ src/components/Tours.js
@@ -1,4 +1,5 @@
 import { Title } from "./Title"
+import { tours } from "./data.js"
 
 export const Tours = () => {
 
@@ -11,95 +12,32 @@ export const Tours = () => {
                 </div>
 
                 <div className="section-center featured-center">
-                    <article className="tour-card">
-                        <div className="tour-img-container">
-                            <img src="./images/tour-1.jpeg" className="tour-img" alt="" />
-                            <p className="tour-date">august 26th, 2020</p>
-                        </div>
-                        <div className="tour-info">
-                            <div className="tour-title">
-                                <h4>Tibet Adventure</h4>
-                            </div>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
-                                vitae tempore voluptatum maxime reprehenderit eum quod
-                                exercitationem fugit, qui corporis.
-                            </p>
-                            <div className="tour-footer">
-                                <p>
-                                    <span><i className="fas fa-map"></i></span> china
-                                </p>
-                                <p>6 days</p>
-                                <p>from $2100</p>
-                            </div>
-                        </div>
-                    </article>
 
-                    <article className="tour-card">
-                        <div className="tour-img-container">
-                            <img src="./images/tour-2.jpeg" className="tour-img" alt="" />
-                            <p className="tour-date">october 1th, 2020</p>
-                        </div>
-                        <div className="tour-info">
-                            <h4>best of java</h4>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
-                                vitae tempore voluptatum maxime reprehenderit eum quod
-                                exercitationem fugit, qui corporis.
-                            </p>
-                            <div className="tour-footer">
-                                <p>
-                                    <span><i className="fas fa-map"></i></span> indonesia
-                                </p>
-                                <p>11 days</p>
-                                <p>from $1400</p>
+                    {tours.map((t) => {
+                        return <article key={t.id} className="tour-card">
+                            <div className="tour-img-container">
+                                <img src={t.img} className="tour-img" alt="" />
+                                <p className="tour-date">{t.date}</p>
                             </div>
-                        </div>
-                    </article>
+                            <div className="tour-info">
+                                <div className="tour-title">
+                                    <h4>{t.title}</h4>
+                                </div>
+                                <p>
+                                    {t.text}
+                                </p>
+                                <div className="tour-footer">
+                                    <p>
+                                        <span><i className="fas fa-map"></i></span> {t.location}
+                                    </p>
+                                    <p>{t.length}</p>
+                                    <p>{t.price}</p>
+                                </div>
+                            </div>
+                        </article>
 
-                    <article className="tour-card">
-                        <div className="tour-img-container">
-                            <img src="./images/tour-3.jpeg" className="tour-img" alt="" />
-                            <p className="tour-date">september 15th, 2020</p>
-                        </div>
-                        <div className="tour-info">
-                            <h4>explore hong kong</h4>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
-                                vitae tempore voluptatum maxime reprehenderit eum quod
-                                exercitationem fugit, qui corporis.
-                            </p>
-                            <div className="tour-footer">
-                                <p>
-                                    <span><i className="fas fa-map"></i></span> hong kong
-                                </p>
-                                <p>8 days</p>
-                                <p>from $5000</p>
-                            </div>
-                        </div>
-                    </article>
+                    })}
 
-                    <article className="tour-card">
-                        <div className="tour-img-container">
-                            <img src="./images/tour-4.jpeg" className="tour-img" alt="" />
-                            <p className="tour-date">december 5th, 2019</p>
-                        </div>
-                        <div className="tour-info">
-                            <h4>kenya highlights</h4>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
-                                vitae tempore voluptatum maxime reprehenderit eum quod
-                                exercitationem fugit, qui corporis.
-                            </p>
-                            <div className="tour-footer">
-                                <p>
-                                    <span><i className="fas fa-map"></i></span> kenya
-                                </p>
-                                <p>20 days</p>
-                                <p>from $3300</p>
-                            </div>
-                        </div>
-                    </article>
                 </div>
             </section></>
     )
